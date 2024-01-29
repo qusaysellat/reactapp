51e473e10ace8500be02e5adc5a8b05ce0eb1d4f
qusaysellat <qusaysellat123@gmail.com>
2024-01-29 23:32:23 +0400

    first push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First React Project On My Github!
         </p>
         <a
           className="App-link"
