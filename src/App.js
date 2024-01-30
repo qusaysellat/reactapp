2073dadf0d19a131a715924cfefd5c5c254957a9
qusaysellat <qusaysellat123@gmail.com>
2024-01-30 08:43:46 +0400

    change in comment content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First React Project On My Github!
+          First ReactJs Project On My Github!
         </p>
         <a
           className="App-link"
